baa7336ae4785eaea1c707dc97707fbd921d7716
Mulin2021 <BMulin@mail.ru>
2021-12-04 20:42:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def pro(qwer,wer='wer'):
+    print(wer)
+    print(qwer)
+
+    return 'возрат', 'и еще'
+
+
+ss=print(pro('позиционный '))
+print(ss)# None
+print(pro('qwer'))
